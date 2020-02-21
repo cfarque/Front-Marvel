f2cfd52718cad4514751d8713aae5df5cc03d39c
Claire <ptixibit@hotmail.fr>
2020-02-21 09:50:56 +0100

    deploy

--- src/Containers/Characters/Characters.js
+++ src/Containers/Characters/Characters.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Character from "../Containers/Characters/Characters.js";
+import Character from "src/Components/Characters/Character.js";
 import "./Characters.css";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
